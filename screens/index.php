c75ea560864190f35db67d727789f14974bb4cb0
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-20 00:38:32 +0300

    Update index.php

--- screens/index.php
+++ screens/index.php
@@ -1,6 +1,7 @@
 <?php 
     include 'C:\xampp\htdocs\to-do-app\components\navbar.php';
     require_once '../components/db.php';
+    session_start();
 
     $sql = "SELECT id, task FROM tasks";
     $result = $conn->query($sql);
@@ -21,7 +22,7 @@
                     <?php 
                     while ($row = $result->fetch_assoc()) {
                         $task = $row['task'];
-                        $id = $row['id'];
+                        $id = $_SESSION = $row['id'];
                         echo '<tr>
                         <td class="col ">
                             <div class="form-check">
@@ -35,44 +36,25 @@
                             <form action = "../screens/edit.php" method = "POST">
                             <button type="submit" name = "edit" class="btn btn-warning">Edit</button>
                             <button type="button" class="btn btn-danger">Delete</button>
+                            <input type="hidden"  name="user_id" value="'. $id.'">
+
                          </form>
                             </td>
-                    </tr>';
-    }
+                        </tr>';
+                    }
 
 
                     ?> 
-                        <tr>
-                            <td class="col ">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                                </div>
-                            </td>
-                            <td class="col-8 ">
-                                <p class="text-center fs-4">Task texst</p>
-                            </td>
-                            <td class="col-3 ">
-                                <button type="button" class="btn btn-warning">Edit</button>
-                                <button type="button" class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
+                        
 
                     </tbody>
                 </table>
             </table>
         </div>
-       
-        <button type="submit" name = "add" class="btn btn-danger">create new</button>
-    <?php 
-        if(isset($_GET['add'])) {
-                echo "Strādā";
-                header("Location:../screens/create.php");
-            } else {
-                echo "error";
-            }
+        <form action = "../screens/create.php" method ="POST"> 
+        <button type="submit" name = "add" class="btn btn-primary btn-lg"> + create new</button>
+        </form>
     
-    
-    ?> 
 </div>
 
 <?php 
