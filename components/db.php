64d036155f2b3f2c6eeedbc8ea23938bfafce2c5
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-20 00:10:03 +0300

    Create db.php

---
+++ components/db.php
@@ -0,0 +1,16 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $pass = "";
+    $dbname = "to-do-list";
+
+    //conection to server
+
+    $conn = mysqli_connect($servername, $username, $pass, $dbname);
+
+    // check conection
+
+    if (!$conn) {
+    die("Error:" . mysqli_connect_error());
+    }
