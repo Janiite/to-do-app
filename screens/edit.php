fc3f05daff82cfb65078f653e3ae2cb5a3ccbce3
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-20 00:10:27 +0300

    Create edit.php

---
+++ screens/edit.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style\style.css" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+
+    <title>Edit tasks</title>
+</head>
+
+<body>
+    <div class="box">
+        <h1>Edit task</h1>
+        <div class="newTask">
+
+            <form class="container mt-5">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="task">
+                    <label for="floatingInput">Task</label>
+                </div>
+
+                <button type="update" class="btn btn-primary mt-5">Update</button>
+            </form>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
