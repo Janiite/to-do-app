a9f7624178e4063c10903a95ec3b0a776aa36337
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-25 20:37:46 +0300

    Create delete.php

---
+++ screens/delete.php
@@ -0,0 +1,13 @@
+<?php 
+// conectien to database
+    require_once "../components/db.php";
+// deleting task from table 
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $dsql = "DELETE FROM tasks WHERE id = $id";
+        $result = $conn->query($dsql);
+        if (!$result) {
+            die('error');
+        }
+            header("Location: ../screens/index.php");
+        }
