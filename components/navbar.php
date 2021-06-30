11c5ad4ff04a134e195632bdc73c6e266c71ea1c
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-30 17:34:41 +0300

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,14 @@
+<?php session_start();
+
+if (!isset($_SESSION['userid'])){
+    header("Location:login.php");
+}
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header("Location: ../screens/login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,18 +22,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
 
-    <title>Your To Do List</title>
+    <title> Your To Do List</title>
 </head>
 
 <body>
     <!--Nav bar-->
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand px-5">Users To Do List</a>
+            <a class="navbar-brand px-5"><?php echo $_SESSION['logname'];?> To Do List</a>
             
 
-            <form class="d-flex px-5">
-                <button class="btn btn-outline-success" type="submit">Log Out</button>
+            <form action = "" method = "POST" class="d-flex px-5">
+                <button name = "logout" class="btn btn-outline-success" type="submit">Log Out</button>
             </form>
         </div>
     </nav>
