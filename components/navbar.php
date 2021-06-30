4f13f8bd1b3b9343596336a633db7539f91f093a
Jana Fedotovska <janafedotovska@gmail.com>
2021-06-30 21:58:02 +0300

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -16,6 +16,10 @@ if (isset($_POST['logout'])) {
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../style/style.css" />
 
     <!-- Bootstrap CSS -->
@@ -27,13 +31,15 @@ if (isset($_POST['logout'])) {
 
 <body>
     <!--Nav bar-->
-    <nav class="navbar navbar-light bg-light">
+    <div class="box2">
+    <nav class="navbar navbar-light bg-light py-2 ">
         <div class="container-fluid">
-            <a class="navbar-brand px-5"><?php echo $_SESSION['logname'];?> To Do List</a>
+            <a class="navbar-brand px-5 fs-1 " ><i class="fas fa-calendar-check"></i>  <?php echo $_SESSION['logname'];?> To Do List</a>
             
 
             <form action = "" method = "POST" class="d-flex px-5">
-                <button name = "logout" class="btn btn-outline-success" type="submit">Log Out</button>
+                <button name = "logout" class="btn btn-outline-primary" type="submit">Log Out</button>
             </form>
         </div>
     </nav>
+</div>
